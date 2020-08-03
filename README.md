e563385661f74a13bc1f9216789d6b29ccdd83d5
Tam Trinh <contact.tamsoft@gmail.com>
2020-08-03 16:58:21 +0700

    Created

---
+++ README.md
@@ -0,0 +1,85 @@
+# node-filesystem-cache
+Module cache for NodeJs using Filesystem.
+
+## Installation
+```
+npm install node-filesystem-cache --save
+```
+
+## Configuration
+```
+const CacheApi = require('node-filesystem-cache');
+const cachePath = __dirname + '/cache';
+const Cache = new CacheApi(cachePath);
+```
+
+## Cache Usage
+### Retrieving Items From The Cache
+The get method on the Cache facade is used to retrieve items from the cache. If the item does not exist in the cache, null will be returned. If you wish, you may pass a second argument to the get method specifying the default value you wish to be returned if the item doesn't exist:
+```
+value = Cache.get('key');
+value = Cache.get('key', 'default');
+```
+
+### Checking For Item Existence
+The has method may be used to determine if an item exists in the cache. This method will return false if the value is null:
+```
+if (Cache.has('key')) {
+    //
+}
+```
+### Retrieve & Store
+Sometimes you may wish to retrieve an item from the cache, but also store a default value if the requested item doesn't exist.
+```
+// Synchronous
+value = Cache.remember('users', seconds, () => {
+     return [
+        {
+            id: 1,
+            name: 'User 1',
+        },
+        {
+            id: 2,
+            name: 'User 2',
+        }
+     ];
+ });
+
+// Asynchronous
+value = await Cache.rememberAsync('users', seconds, async () => {
+     return await userRepository.getUsers();
+ });
+```
+### Retrieve & Delete
+If you need to retrieve an item from the cache and then delete the item, you may use the pull method. Like the get method, null will be returned if the item does not exist in the cache:
+```
+value = Cache.pull('key');
+```
+### Storing Items In The Cache
+You may use the put method on the Cache to store items in the cache:
+```
+Cache.put('key', 'value', seconds);
+```
+If the storage time is not passed to the put method, the item will be stored indefinitely:
+```
+Cache.put('key', 'value');
+```
+### Store If Not Present
+The add method will only add the item to the cache if it does not already exist in the cache store. The method will return true if the item is actually added to the cache. Otherwise, the method will return false:
+```
+Cache.add('key', 'value', seconds);
+```
+### Storing Items Forever
+The forever method may be used to store an item in the cache permanently. Since these items will not expire, they must be manually removed from the cache using the forget method:
+```
+Cache.forever('key', 'value');
+```
+## Removing Items From The Cache
+You may remove items from the cache using the forget method:
+```
+Cache.forget('key');
+```
+You may clear the entire cache using the clear method:
+```
+Cache.clear();
+```
---
+++ test/cacheTest.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const assert = require('assert');
+const http = require('http');
+const request = require('supertest');
+const CacheApi = require('..');
+const cachePath = __dirname + '/cache';
+
+if(!fs.existsSync(cachePath)) {
+    fs.mkdirSync(cachePath, {
+        mode: 0o777,
+    });
+}
+
+const Cache = new CacheApi(cachePath);
+
+const seconds = 20000;
+const userObject = {
+    id: 1,
+    name: 'tamtrinh',
+    email: 'tam.maris94@gmail.com',
+    phone: '0342.831.832',
+};
+const userCallbackObject = {
+    id: 2,
+    name: 'tamtrinh_callback',
+    email: 'tam.maris94@gmail.com',
+    phone: '0342.831.832',
+};
+
+function serverApi() {
+    const server = http.createServer(function (req, res) {
+        res.end(JSON.stringify(userCallbackObject))
+    });
+    return request(server).get('/').set('Accept', 'application/json');
+}
+
+describe('Cache', function () {
+    it('Store If Not Present', function () {
+        const value = Cache.add('test_cache', userObject, seconds);
+        assert.strictEqual(value, true);
+        const value_exists = Cache.add('test_cache', userObject, seconds);
+        assert.strictEqual(value_exists, false);
+    });
+    it('Checking For Item Existence', function () {
+        assert.strictEqual(Cache.has('test_cache'), true);
+        assert.strictEqual(Cache.has('test_cache_add_not'), false);
+    });
+    it('Retrieve & Store', function () {
+        assert.deepStrictEqual(Cache.get('test_cache'), userObject);
+    });
+    it('Removing Items From The Cache', function () {
+        Cache.forget('test_cache');
+        assert.strictEqual(Cache.has('test_cache'), false);
+    });
+    it('Storing Items Forever', function () {
+        const value = Cache.forever('test_cache', userObject);
+        assert.deepStrictEqual(value, userObject);
+    });
+    it('Retrieve & Delete', function () {
+        const value = Cache.pull('test_cache');
+        assert.deepStrictEqual(value, userObject);
+        assert.strictEqual(Cache.has('test_cache'), false);
+    });
+
+    it('Store Synchronous', async function () {
+        Cache.clear();
+        assert.strictEqual(Cache.has('test_cache_callback'), false);
+
+        const value = Cache.remember('test_cache_callback', seconds, () => {
+            return userCallbackObject
+        });
+
+        assert.deepStrictEqual(value, userCallbackObject);
+        assert.strictEqual(Cache.has('test_cache_callback'), true);
+        assert.deepStrictEqual(Cache.get('test_cache_callback'), userCallbackObject);
+    });
+
+    it('Store Asynchronous', async function () {
+        Cache.clear();
+        assert.strictEqual(Cache.has('test_cache_callback'), false);
+
+        const value = await Cache.rememberAsync('test_cache_callback', seconds, async () => {
+            const response = await serverApi();
+            return JSON.parse(response.text);
+        });
+
+        assert.deepStrictEqual(value, userCallbackObject);
+        assert.strictEqual(Cache.has('test_cache_callback'), true);
+        assert.deepStrictEqual(Cache.get('test_cache_callback'), userCallbackObject);
+    });
+
+});
+
+describe('Remove', function () {
+    it('Removing All Items From The Cache', function () {
+        Cache.clear();
+        assert.strictEqual(Cache.has('test_cache'), false);
+        assert.strictEqual(Cache.has('test_cache_callback'), false);
+    });
+});
